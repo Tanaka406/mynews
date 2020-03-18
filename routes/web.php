a9ef0093e47ed73490b09a8281a23cc6ffe0d113
Tanaka_Yusuke <yusuke.kun2400@gmail>
2020-03-18 12:24:37 +0000

    text kadai21

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
 //課題3
@@ -29,3 +31,6 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
