7cd7f71a588987da41aecff84ca6b2ad15c65fac
Tanaka_Yusuke <yusuke.kun2400@gmail>
2020-03-23 12:55:07 +0000

    text kadai23

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -14,6 +15,10 @@ class ProfileController extends Controller
 
     public function create()
     {
+        $this->validate($request, Profile::$rules);
+
+      $Profile = new Profile;
+      $form = $request->all();
         return redirect('admin/profile/create');
     }
 
