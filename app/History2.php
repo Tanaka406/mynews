9923af0f9dcd9725183c53781322b674c5d7efb5
Tanaka_Yusuke <yusuke.kun2400@gmail>
2020-04-02 12:01:38 +0000

    text kadai27

--- app/History2.php
+++ app/History2.php
@@ -4,20 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Profile extends Model
+class History2 extends Model
 {
-     protected $guarded = array('id');
+    protected $guarded = array('id');
 
-    // 以下を追記
     public static $rules = array(
         'name' => 'required',
         'gender' => 'required',
         'hobby' => 'required',
         'introduction'=> 'required',
-    );
-     public function histories()
-    {
-      return $this->hasMany('App\History2');
-
 }
-
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -31,7 +31,7 @@
                     <div class="form-group row">
                         <label class="col-md-2" for="hobby">趣味</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobby }}">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobbyA }}">
                         </div>
                     </div>
                     <div class="form-group row">
